4a3e8cc6290e5f050c64b8db150f226f256c1db5
LucLonjon <luc.lonjon@gmail.com>
2021-12-04 12:10:31 +0100

    java connected back

--- webuy-app/src/app/classes/product-models-annonce.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ProductModelsAnnonce } from './product-models-annonce';
-
-describe('ProductModelsAnnonce', () => {
-  it('should create an instance', () => {
-    expect(new ProductModelsAnnonce()).toBeTruthy();
-  });
-});
